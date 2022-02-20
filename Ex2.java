b9668424a4a561be731df75f7a92d2ef2595aa18
Pavithra Meganathan <66105765+Pavithra-fireflies14@users.noreply.github.com>
2022-02-20 10:16:11 +0530

    New commit

--- Ex2.java
+++ Ex2.java
@@ -6,5 +6,6 @@ public static void main(String args[])
    System.out.println("After first commit");
    System.out.println("after first  push ");
    System.out.println("final commit");
+   System.out.println("New commit");
 }
 }
